09fe0777f6908d8f7167a02bc5a4de4dbf2fc17e
RafaPach <rafael_eli_pacheco@hotmail.com>
2022-06-21 10:45:56 +0100

    Express

---
+++ my-app/app.js
@@ -0,0 +1,30 @@
+import express from 'express';
+const app = express();
+const PORT = process.env.port || 3000;
+import cors from 'cors';
+
+// midleware to parse json ✅
+// import router to anime ✅
+// route /anime ✅
+
+app.use(express.json());
+app.use(cors());
+// app.use(express.static('public'));
+
+// test route
+app.get('/', function (req, res) {
+  res.json({
+    success: true,
+    message: 'Test route up and running!',
+  });
+});
+
+// app.use('/anime', routerAnime);
+
+// app.get('/', function (req, res) {
+//   res.sendFile(html);
+// });
+
+app.listen(PORT, function () {
+  console.log(`Server is running on port ${PORT}`);
+});
