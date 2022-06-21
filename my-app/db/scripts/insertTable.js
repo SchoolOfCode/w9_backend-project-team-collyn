ed8c777a008e8db920e7a9a1a23cdc4a9d402d5e
pennyluc <pennyluc321@gmail.com>
2022-06-21 12:02:24 +0100

    inserttable function started

---
+++ my-app/db/scripts/insertTable.js
@@ -0,0 +1,9 @@
+import { query } from '../index.js';
+import { projects } from '../../libs/index.js';
+
+let insert = `INSERT INTO projects (name, project_interest, project_type, strength, availability, img) VALUES ($1, $2, $3, $4, $5, $6)`
+
+async function insertTable() {
+
+   
+}
--- my-app/libs/index.js
+++ my-app/libs/index.js
@@ -1,5 +1,31 @@
 export const projects = [{
-    name: "Penny",
-    project_interest
+    name: "Bob",
+    project_interest: "Sports",
+    project_type: "Blog",
+    strength: "Backend",
+    availability: "Tuesday evenings",
+    img: ""
+    },
+  {  name: "Jim",
+    project_interest: "Games",
+    project_type: "Games",
+    strength: "Frontend",
+    availability: "Weekday evenings",
+    img: ""
+},
+  {  name: "Ella",
+    project_interest: "Photography",
+    project_type: "Gallery",
+    strength: "Backend",
+    availability: "Weekend mornings",
+    img: ""
+},
+  {  name: "Sally",
+    project_interest: "Fashion",
+    project_type: "Forum",
+    strength: "Testing",
+    availability: "Weekend allday",
+    img: ""
+}
 
-}];
+];
